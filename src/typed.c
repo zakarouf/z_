ba579d5a7352aac0f0d09cb40ab8866dc848f30f
zakarouf <zak.d.official@gmail.com>
2021-03-14 23:07:02 +0530

    type specific array functions .c

--- src/typed.c
+++ src/typed.c
@@ -109,6 +109,330 @@ void z__ptrArr_delete(z__ptrArr* arrt, z__bool nameFree)
 }
 
 
+#ifdef Z___TYPE_CONFIG__USE_TYPE_ARRAYS
+z__i8Arr z__i8Arr_create(z__u32 len)
+{
+    return (z__i8Arr) {
+        .data = malloc(sizeof (z__i8) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__i8Arr_push( z__i8Arr *arr, z__i8 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data,  sizeof(z__i8)* (arr->len) );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__i16Arr z__i16Arr_create(z__u32 len)
+{
+    return (z__i16Arr) {
+        .data = malloc(sizeof(z__i16) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__i16Arr_push( z__i16Arr *arr, z__i16 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__i16)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__i32Arr z__i32Arr_create(z__u32 len)
+{
+    return (z__i32Arr) {
+        .data = malloc(sizeof(z__i32) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__i32Arr_push( z__i32Arr *arr, z__i32 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__i32)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__i64Arr z__i64Arr_create(z__u32 len)
+{
+    return (z__i64Arr) {
+        .data = malloc(sizeof(z__i64) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__i64Arr_push( z__i64Arr *arr, z__i64 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__i64)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__u8Arr z__u8Arr_create(z__u32 len)
+{
+    return (z__u8Arr) {
+        .data = malloc(sizeof(z__u8) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__u8Arr_push( z__u8Arr *arr, z__u8 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__u8)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__u16Arr z__u16Arr_create(z__u32 len)
+{
+    return (z__u16Arr) {
+        .data = malloc(sizeof(z__u16) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__u16Arr_push( z__u16Arr *arr, z__u16 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__u16)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__u32Arr z__u32Arr_create(z__u32 len)
+{
+    return (z__u32Arr) {
+        .data = malloc(sizeof(z__u32) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__u32Arr_push( z__u32Arr *arr, z__u32 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__u32)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__u64Arr z__u64Arr_create(z__u32 len)
+{
+    return (z__u64Arr) {
+        .data = malloc(sizeof(z__u64) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__u64Arr_push( z__u64Arr *arr, z__u64 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__u64)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__f32Arr z__f32Arr_create(z__u32 len)
+{
+    return (z__f32Arr) {
+        .data = malloc(sizeof(z__f32) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__f32Arr_push( z__f32Arr *arr, z__f32 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__f32)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+z__f64Arr z__f64Arr_create(z__u32 len)
+{
+    return (z__f64Arr) {
+        .data = malloc(sizeof(z__f64) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__f64Arr_push( z__f64Arr *arr, z__f64 val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__f64)*arr->len );
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+
+z__boolArr z__boolArr_create(z__u32 len)
+{
+    return (z__boolArr) {
+        .data = malloc(sizeof(z__bool) * len),
+        .len = len,
+        .lenUsed = 0
+    };
+}
+void z__boolArr_push( z__boolArr *arr, z__bool val)
+{
+    if (arr->lenUsed >= arr->len)
+    {
+        arr->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
+        arr->data = safe_realloc(arr->data, sizeof(z__bool) *arr->len);
+    }
+    arr->data[arr->lenUsed] = val;
+    arr->lenUsed += 1;
+}
+
+
+
+void z__i8Arr_resize( z__i8Arr *arr, z__i8 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__i8));
+}
+void z__i16Arr_resize( z__i16Arr *arr, z__i16 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__i16));
+}
+void z__i32Arr_resize( z__i32Arr *arr, z__i32 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__i32));
+}
+void z__i64Arr_resize( z__i64Arr *arr, z__i64 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__i64));
+}
+void z__u8Arr_resize( z__u8Arr *arr, z__u8 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__u8));
+}
+void z__u16Arr_resize( z__u16Arr *arr, z__u16 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__u16));
+}
+void z__u32Arr_resize( z__u32Arr *arr, z__u32 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__u32));
+}
+void z__u64Arr_resize( z__u64Arr *arr, z__u64 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__u64));
+}
+void z__f32Arr_resize( z__f32Arr *arr, z__f32 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__f32));
+}
+void z__f64Arr_resize( z__f64Arr *arr, z__f64 newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__f64));
+}
+void z__boolArr_resize( z__boolArr *arr, z__bool newSize)
+{
+    arr->len = newSize;
+    if (arr->lenUsed > arr->len)
+    {
+        arr->lenUsed = arr->len;
+    }
+    arr->data = safe_realloc(arr->data, arr->len * sizeof(z__bool));
+}
+
+#endif //#ifdef Z___TYPE_CONFIG__USE_TYPE_ARRAYS
 
 
 
