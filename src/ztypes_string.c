c78e70b72cc18954d59890a2ab1a22118ca80dc7
zakarouf <zak.d.official@gmail.com>
2021-03-29 23:43:33 +0530

    Changed: Replaced char with z__char to match future typesafety.

--- src/ztypes_string.c
+++ src/ztypes_string.c
@@ -141,7 +141,7 @@
 	    long fsize = ftell(f);
 	    fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
 
-	    char *string = malloc(fsize + 1);
+	    z__char *string = malloc(fsize + 1);
 	    fread(string, 1, fsize, f);
 	    fclose(f);
 
