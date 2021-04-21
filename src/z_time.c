709e31d9b0014568264076dddb5800ef9c3bd60e
zakarouf <zak.d.official@gmail.com>
2021-04-21 20:29:36 +0530

    Added: Indentation.

--- src/z_time.c
+++ src/z_time.c
@@ -1,29 +1,29 @@
 #include <sys/time.h>
 
-
 #ifdef WIN32
-#include <windows.h>
+    #include <windows.h>
 #elif _POSIX_C_SOURCE >= 199309L
-#include <time.h>   // for nanosleep
+    #include <time.h>   // for nanosleep
 #else
-#include <unistd.h> // for usleep
+    #include <unistd.h> // for usleep
 #endif
 
 
 void z__time_msleep(int milliseconds)
 { // cross-platform sleep function
-#ifdef WIN32
-    Sleep(milliseconds);
-#elif _POSIX_C_SOURCE >= 199309L
-    struct timespec ts;
-    ts.tv_sec = milliseconds / 1000;
-    ts.tv_nsec = (milliseconds % 1000) * 1000000;
-    nanosleep(&ts, NULL);
-#else
-    if (milliseconds >= 1000)
-      sleep(milliseconds / 1000);
-    usleep((milliseconds % 1000) * 1000);
-#endif
+    #ifdef WIN32
+        Sleep(milliseconds);
+    #elif _POSIX_C_SOURCE >= 199309L
+        struct timespec ts;
+        ts.tv_sec = milliseconds / 1000;
+        ts.tv_nsec = (milliseconds % 1000) * 1000000;
+        nanosleep(&ts, NULL);
+    #else
+        if (milliseconds >= 1000) { 
+          sleep(milliseconds / 1000);
+        }
+        usleep((milliseconds % 1000) * 1000);
+    #endif
 }
 
 int z__time_getLocalTime(int *h, int *m, int *s)
