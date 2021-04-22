fbe74653f754de340e1aec0ffbf80b98a0118a7f
zakarouf <zak.d.official@gmail.com>
2021-04-22 17:02:52 +0530

    Fixed: CAT

--- src/prep/zprep.h
+++ src/prep/zprep.h
@@ -5,13 +5,13 @@
 #define ZAKAROUF__ZPP_MINOR 1
 #define ZAKAROUF__ZPP_PATCH 0
 
-#define zpp__CAT_PRIMITIVE(a, b)        a##b
-#define zpp__CAT3_PRIMITIVE(a, b, c)    a##b##c
-#define zpp__CAT4_PRIMITIVE(a, b, c, d) a##b##c##d
+#define zpp__PRIMITIVE_CAT(a, b)        a##b
+#define zpp__PRIMITIVE_CAT3(a, b, c)    a##b##c
+#define zpp__PRIMITIVE_CAT4(a, b, c, d) a##b##c##d
 
-#define zpp__CAT(a, b)          zpp__CAT_PRIMITIVE(a, b) 
-#define zpp__CAT3(a, b, c)      zpp__CAT3_PRIMITIVE(a, b) 
-#define zpp__CAT4(a, b, c, d)   zpp__CAT4_PRIMITIVE(a, b) 
+#define zpp__CAT(a, b)          zpp__PRIMITIVE_CAT(a, b) 
+#define zpp__CAT3(a, b, c)      zpp__PRIMITIVE_CAT3(a, b, c) 
+#define zpp__CAT4(a, b, c, d)   zpp__PRIMITIVE_CAT4(a, b, c, d) 
 
 
 #define zpp__PRIV__VARIADICS_COUNT_AUX(                                             \
@@ -23,7 +23,7 @@
 
 #define zpp__PRIV__VARIADICS_COUNT(...)                                             \
   zpp__PRIV__VARIADICS_COUNT_AUX(                                                   \
-          __VA_ARGS__, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49   \
+         __VA_ARGS__, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49   \
         , 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31    \
         , 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13    \
         , 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, ~)
@@ -31,6 +31,7 @@
 
 #define zpp__Args_Count(...) zpp__PRIV__VARIADICS_COUNT(__VA_ARGS__)
 #define zpp__Args_Overload(N, ...) zpp__CAT(N, zpp__PRIV__VARIADICS_COUNT(__VA_ARGS__) ) (__VA_ARGS__)
+#define zpp__Args_OverloadCustom zpp__PRIV__VARIADICS_COUNT_AUX
 
 #define zpp__asset_eq __builtin_choose_expr
 
