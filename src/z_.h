82324303511493f78a42cd065872bffa2a83c283
zakarouf <zak.d.official@gmail.com>
2021-04-22 20:51:33 +0530

    Added: zprep to z_.h header.

--- src/z_.h
+++ src/z_.h
@@ -3,9 +3,9 @@
 
 #define ZAKAROUF__Z__VERSION_MAJOR 2
 #define ZAKAROUF__Z__VERSION_MINOR 7
-#define ZAKAROUF__Z__VERSION_PATCH 1
+#define ZAKAROUF__Z__VERSION_PATCH 2
 
-
+#include "prep/zprep.h"
 #include "types/ztypes.h"
 #include "extra/zextra.h"
 
