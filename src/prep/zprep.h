c4737666fcbf2b7430dca1139dee6109cb4fb6af
zakarouf <zak.d.official@gmail.com>
2021-04-22 23:06:40 +0530

    Update: Pushed zprep version.

--- src/prep/zprep.h
+++ src/prep/zprep.h
@@ -1,9 +1,9 @@
 #ifndef ZAKAROUF__ZPREP_H
 #define ZAKAROUF__ZPREP_H
 
-#define ZAKAROUF__ZPP_MAJOR 0
-#define ZAKAROUF__ZPP_MINOR 2
-#define ZAKAROUF__ZPP_PATCH 0
+#define ZAKAROUF__ZPP_VERSION_MAJOR 0
+#define ZAKAROUF__ZPP_VERSION_MINOR 2
+#define ZAKAROUF__ZPP_VERSION_PATCH 1
 
 
 #include "zprep/base.h"
