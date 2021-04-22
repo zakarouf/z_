d9d39ae2e1baf19389865380c92408221a54f5a4
zakarouf <zak.d.official@gmail.com>
2021-04-22 23:07:58 +0530

    Rename: zprep: Assert Compiler Dependent: Header Guard

--- src/prep/zprep/assert_comp.h
+++ src/prep/zprep/assert_comp.h
@@ -1,5 +1,5 @@
-#ifndef ZAKAROUF__ZPREP_ASSERT_H
-#define ZAKAROUF__ZPREP_ASSERT_H
+#ifndef ZAKAROUF__ZPREP_ASSERT_COMPILER_H
+#define ZAKAROUF__ZPREP_ASSERT_COMPILER_H
 
 #define zpp__asset_eq __builtin_choose_expr
 
