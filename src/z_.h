811838e21e23c43885c65fa2e6cbba012d1d8eb1
zakarouf <zak.d.official@gmail.com>
2021-05-01 23:39:56 +0530

    Update: Pushed Version to ztypes::3.3.0 and z_::3.5.0

--- src/z_.h
+++ src/z_.h
@@ -2,8 +2,8 @@
 #define ZAKAROUF_Z__H
 
 #define ZAKAROUF__Z__VERSION_MAJOR 3
-#define ZAKAROUF__Z__VERSION_MINOR 4
-#define ZAKAROUF__Z__VERSION_PATCH 4
+#define ZAKAROUF__Z__VERSION_MINOR 5
+#define ZAKAROUF__Z__VERSION_PATCH 0
 
 #include "prep/zprep.h"
 #include "types/ztypes.h"
