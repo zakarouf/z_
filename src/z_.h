8bd7f84681de190986ebcbeb794471a07a1b18f5
zakarouf <zak.d.official@gmail.com>
2021-05-02 11:58:47 +0530

    Update: Pushed Version: ztypes -> 3.10.1, z_ -> 3.15.0

--- src/z_.h
+++ src/z_.h
@@ -2,7 +2,7 @@
 #define ZAKAROUF_Z__H
 
 #define ZAKAROUF__Z__VERSION_MAJOR 3
-#define ZAKAROUF__Z__VERSION_MINOR 5
+#define ZAKAROUF__Z__VERSION_MINOR 15
 #define ZAKAROUF__Z__VERSION_PATCH 0
 
 #include "prep/zprep.h"
