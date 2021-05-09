e858c3de4bf8a2b3ad8482c639a914bce42a2a18
zakarouf <zak.d.official@gmail.com>
2021-05-09 23:07:56 +0530

    Update: prep: zpp_Args_get_[n] => Completed 1 -> 64 template.

--- src/prep/zprep.h
+++ src/prep/zprep.h
@@ -3,7 +3,7 @@
 
 #define ZAKAROUF__ZPP_VERSION_MAJOR 0
 #define ZAKAROUF__ZPP_VERSION_MINOR 3
-#define ZAKAROUF__ZPP_VERSION_PATCH 1
+#define ZAKAROUF__ZPP_VERSION_PATCH 2
 
 
 #include "zprep/base.h"
