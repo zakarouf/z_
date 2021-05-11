37984845ec5fc044140aaf3382d18a5cbc9b1b01
zakarouf <zak.d.official@gmail.com>
2021-05-11 17:34:49 +0530

    Added: typeof: header

--- src/types/types/fnptr.h
+++ src/types/types/fnptr.h
@@ -1,6 +1,8 @@
 #ifndef ZAKAROUF__ZTYPES_TYPES__FNPTR_H
 #define ZAKAROUF__ZTYPES_TYPES__FNPTR_H
 
+#include "typeof.h"
+
 #define z__fnptr_fn(fn) z__typeof(fn)*
 #define z__fnptr(Name, T, ...) T(*Name)(__VA_ARGS__)
 
