a62b6dbc0e7aeebb74508e4fcbcde3edc325cd77
zakarouf <zak.d.official@gmail.com>
2021-05-11 17:46:18 +0530

    Init: proc.

---
+++ src/proc/proc/omp.h
@@ -0,0 +1,6 @@
+#ifndef ZAKAROUF__Z__PROC_OMP_H
+#define ZAKAROUF__Z__PROC_OMP_H
+
+#include <omp.h>
+
+#endif
---
+++ src/proc/proc/pt.h
@@ -0,0 +1,6 @@
+#ifndef ZAKAROUF__Z__PROC_PT_H
+#define ZAKAROUF__Z__PROC_PT_H
+
+#include <pthread.h>
+
+#endif
