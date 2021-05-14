4bca1df92e81908bf0e7009377b7c5d13d1e35c2
zakarouf <zak.d.official@gmail.com>
2021-05-14 11:00:15 +0530

    Added: PreP: Tertianary operatort

--- src/prep/prep/assert.h
+++ src/prep/prep/assert.h
@@ -22,7 +22,16 @@
 #define zpp__doif_construct_post(condition, method, ...)\
     if(condition)\
         for(int keep = 1; keep; keep ^= 1, method(__VA_ARGS__) )
-            
+
+#define zpp__ter__if(exp)   (exp)?
+#define zpp__ter__elif(exp) :(exp)?
+#define zpp__ter__else      :
+
+#define zpp__ter__op_if(exp)          zpp__ter__if(exp)   (
+#define zpp__ter__op_elif(exp)      ) zpp__ter__elif(exp) (
+#define zpp__ter__op_else           ) zpp__ter__else      (
+#define zpp__ter__op_endif          )
+          
 // Panic
 
 #define zpp__panicif(condition)\
