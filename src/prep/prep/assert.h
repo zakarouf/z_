2820a346203c93749f54353563bd79ff23d1accd
zakarouf <zak.d.official@gmail.com>
2021-05-14 11:01:29 +0530

    Rename: PreP: Assert Construct Method

--- src/prep/prep/assert.h
+++ src/prep/prep/assert.h
@@ -4,10 +4,10 @@
 #define zpp__assert_exp(exp)\
     ((void) ((exp) ? 1 : 0)
 
-#define zpp__assert_exp_method(exp, exception_method_fn, ...)\
+#define zpp__assert_exp_construct(exp, exception_method_fn, ...)\
     ((void) ((exp) ? ((void)0) : exception_method_fn (__VA_ARGS__)))
 
-#define zpp__assert_not_exp_method(exp, exception_method_fn, ...)\
+#define zpp__assert_not_exp_construct(exp, exception_method_fn, ...)\
     ((void) ((exp) ? exception_method_fn (__VA_ARGS__)) : ((void)0) )
 
 #define zpp__assert_eq_ptr(x, y) ( x==y?true:false )
