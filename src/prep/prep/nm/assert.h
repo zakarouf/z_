af76eb31490831cdd6f5fe7296b0860c09a1ac28
zakarouf <zak.d.official@gmail.com>
2021-05-16 21:48:06 +0530

    Fixed: PreP: nm: assert: panicif used to call panic_calls if the assertion was false, [double negetives].

--- src/prep/prep/nm/assert.h
+++ src/prep/prep/nm/assert.h
@@ -4,18 +4,18 @@
 #define zpp__assert_exp(exp)\
     ((void) ((exp) ? 1 : 0)
 
-#define zpp__assert_exp_construct(exp, exception_method_fn, ...)\
-    ((void) ((exp) ? ((void)0) : exception_method_fn (__VA_ARGS__)))
+#define zpp__assert_construct(exp, exception_method_fn, ...)\
+    ( (void)( (exp) ? ((void)0) : exception_method_fn(__VA_ARGS__) ))
 
-#define zpp__assert_not_exp_construct(exp, exception_method_fn, ...)\
-    ((void) ((exp) ? exception_method_fn (__VA_ARGS__)) : ((void)0) )
+#define zpp__assert_not_construct(exp, exception_method_fn, ...)\
+    ( (void)( (exp) ? exception_method_fn(__VA_ARGS__) : ((void)0) ))
 
 #define zpp__assert_eq_ptr(x, y) ( x==y?true:false )
 #define zpp__assert_ptr_checkNULL(x) (x==NULL?true:false)
           
 // Panic
 
-#define zpp__panicif_construct(condition, panic_call, ...) zpp__assert_exp_construct(condition, panic_call, __VA_ARGS__)
+#define zpp__panicif_construct(condition, panic_call, ...) zpp__assert_not_construct(condition, panic_call, __VA_ARGS__)
 
 
 #endif
