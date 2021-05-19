78282f657d9024c80891d3225d9a71cb63d7f5d9
zakarouf <zak.d.official@gmail.com>
2021-05-19 17:51:17 +0530

    Added: Proc: Varied Jobs; Removed & Replaced: z__pt_Context => z__pt_ArgContext.

--- src/proc/pt.h
+++ src/proc/pt.h
@@ -1,8 +1,6 @@
 #ifndef ZAKAROUF__Z__PROC_PT_H
 #define ZAKAROUF__Z__PROC_PT_H
 
-#include <pthread.h>
-
 #include "pt_base.h"
 
 #endif
--- src/proc/pt_base.h
+++ src/proc/pt_base.h
@@ -1,42 +1,46 @@
 #ifndef ZAKAROUF__Z__PROC_PT_BASE_H
 #define ZAKAROUF__Z__PROC_PT_BASE_H
 
+#include <pthread.h>
 
-#define z__pt_Context_Tag(TagN, ...)\
-    struct z__pt_Context__tag##TagN {   \
-        __VA_ARGS__                     \
+#include "../types/base.h"
+
+
+#define z__pt_ArgContext_Tag(TagN, ...)\
+    struct z__pt_ArgContext__tag##TagN {    \
+        z__u32 _id;                         \
+        z__u32 _maxt;                       \
+        __VA_ARGS__;                        \
     }
 
-#define z__pt_ArgContext_Tag(ContextTag, TagN, ...)\
-    struct z__pt_ArgContext__tag##TagN {                \
-        z__u32 id;                                      \
-        struct z__pt_Context__tag##ContextTag *context; \
-        __VA_ARGS__;                                    \
-    }
-
-#define z__pt_Context(TagN)     struct z__pt_Context__tag##TagN
 #define z__pt_ArgContext(TagN)  struct z__pt_ArgContext__tag##TagN
+#define z__pt_ArgContext_assign(arg, memo, val)\
+    {                           \
+        (arg)->memo = (val);    \
+    }
 
+//* Similar Jobs *//
 
-#define z__pt_Job(ArgContext)\
+#define z__pt_Job(ArgContext, ...)\
     struct {                          \
         z__fnptr(*fn, void *, void *);\
-        struct z__pt_ArgContext__tag##ArgContext *Arg;\
+        struct z__pt_ArgContext__tag##ArgContext *Args;\
         pthread_t *Threads;           \
         z__u32 Thread_Num;            \
+        __VA_ARGS__;                  \
     }
 
 #define z__pt_Job_new(Job, n)\
     {                                                                               \
         (Job)->Thread_Num = n;                                                      \
-        (Job)->Arg = z__MALLOC(sizeof(*(Job)->Arg) * (Job)->Thread_Num);                 \
+        (Job)->Args = z__MALLOC(sizeof(*(Job)->Args) * (Job)->Thread_Num);                 \
         (Job)->fn = z__MALLOC(sizeof(*(Job)->fn) * (Job)->Thread_Num);              \
         (Job)->Threads = z__CALLOC(sizeof(*(Job)->Threads), (Job)->Thread_Num);    \
     }
 
 #define z__pt_Job_bind_Arg(Job, ArgContext, of) \
     {                                           \
-        (Job)->Arg[of] = (ArgContext);          \
+        (Job)->Args[of] = (ArgContext);          \
     }
 
 #define z__pt_Job_bind_fn(Job, fn_name, of) \
@@ -44,16 +48,11 @@
         (Job)->fn[of] =  (void*)fn_name;  \
     }
 
-#define z__pt_ArgContext_bind_Context(ArgContext, Context)\
-    {                                   \
-        (ArgContext)->context = Context;\
-    }
-
 #define z__pt_Job_run(Job)\
     {\
         for (int i = 0; i < (Job)->Thread_Num; ++i) {\
-            (Job)->Arg[i].id = i;\
-            pthread_create(&(Job)->Threads[i], NULL, (Job)->fn[i], &(Job)->Arg[i]);\
+            (Job)->Args[i]._id = i;\
+            pthread_create(&(Job)->Threads[i], NULL, (Job)->fn[i], &(Job)->Args[i]);\
         }\
     }\
 
@@ -72,13 +71,76 @@
 
 #define z__pt_Job_delete(Job)\
     {                               \
-        z__FREE((Job)->Arg);        \
+        z__FREE((Job)->Args);        \
         z__FREE((Job)->Threads);    \
         z__FREE((Job)->fn);         \
         (Job)->Thread_Num = 0;      \
     }
 
-#define z__pt_Arg_getContext(Arg) (Arg).context
+
+// Varied Jobs //
+
+typedef
+    struct {                          
+        z__fnptr(*fn, void *, void *);
+        void **Args;                  
+        pthread_t *Threads;           
+        z__u32 Thread_Num;            
+    }z__pt_VarJob;
+
+#define z__pt_VarJob_new(vj , n)\
+    {\
+        (vj)->Thread_Num = n;                                                      \
+        (vj)->Args = z__MALLOC(sizeof(*(vj)->Args) * (vj)->Thread_Num);          \
+        (vj)->fn = z__MALLOC(sizeof(*(vj)->fn) * (vj)->Thread_Num);              \
+        (vj)->Threads = z__CALLOC(sizeof(*(vj)->Threads), (vj)->Thread_Num);     \
+    }
+
+#define z__pt_VarJob_delete(vj)\
+    {                              \
+        z__FREE((vj)->Args);       \
+        z__FREE((vj)->Threads);    \
+        z__FREE((vj)->fn);         \
+        (vj)->Thread_Num = 0;      \
+    }
+
+inline void z__pt_VarJob_set_Arg_id(z__pt_VarJob *vj, z__u32 id, z__u32 of)
+{
+    z__u32 *arg_id = vj->Args[of];
+    *arg_id = id;
+}
+
+#define z__pt_VarJob_bind_Arg(vj, ArgContext, of)\
+    {                                   \
+        (vj)->Args[of] = (ArgContext);  \
+    }
+
+
+#define z__pt_VarJob_bind_fn(vj, fn_name, of) \
+    {                                     \
+        (vj)->fn[of] =  (void*)fn_name;  \
+    }
+
+#define z__pt_VarJob_run(vj)\
+    {\
+        for (int i = 0; i < (vj)->Thread_Num; ++i) {\
+            z__pt_VarJob_set_Arg_id(&vj, i, i);\
+            pthread_create(&(vj)->Threads[i], NULL, (vj)->fn[i], &(vj)->Args[i]);   \
+        }                                                                           \
+    }\
+
+#define z__pt_VarJob_thread_join(vj)\
+    {                                                   \
+        for (int i = 0; i < (vj)->Thread_Num; ++i) {   \
+            pthread_join((vj)->Threads[i] ,NULL);      \
+        }                                               \
+    }
+
+#define z__pt_VarJob_end(vj)\
+    {\
+        z__pt_VarJob_thread_join(vj);\
+        z__pt_VarJob_delete(vj);      \
+    }
 
 
 #endif
