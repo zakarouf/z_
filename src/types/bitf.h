22068a291a24e001b1daaac32850d9c19ce8da7c
zakarouf <zak.d.official@gmail.com>
2021-06-13 13:46:53 +0530

    Changed: Bitf

--- src/types/bitf.h
+++ src/types/bitf.h
@@ -10,17 +10,18 @@
 #define z__BitField_id(Tag, ...)\
         enum{ zpp__Args_maplist_fn_Pattern(z__BitFieldIndex__PRIV__gen, z__BitField_idTag(Tag),, __VA_ARGS__) }
 
-#define z__BitField_raw_set(of, bitIndex)       of |= 1 << (bitIndex)
-#define z__BitField_raw_clear(of, bitIndex)     of &= ~(1 << (bitIndex))
-#define z__BitField_raw_toggle(of, bitIndex)    of ^= 1 << (bitIndex)
-#define z__BitField_raw_is(of, bitIndex)        of & (1 << (bitIndex))
-#define z__BitField_raw_iseq(of, bitIndex)      ((of)&(bitIndex) == bitIndex)
+#define z__bitf_set(of, bitIndex)       of |= 1 << (bitIndex)
+#define z__bitf_clear(of, bitIndex)     of &= ~(1 << (bitIndex))
+#define z__bitf_toggle(of, bitIndex)    of ^= 1 << (bitIndex)
+#define z__bitf_is(of, bitIndex)        of & (1 << (bitIndex))
+#define z__bitf_has(of, bitIndex)       (((of) >> (bitIndex))&1)
+#define z__bitf_iseq(of, bitIndex)      ((of)&(bitIndex) == bitIndex)
 
-#define z__BitField_set(of, tag, bitIndex)      z__BitField_raw_set(of, z__BitField_idTagIn(tag, bitIndex))
-#define z__BitField_clear(of, tag, bitIndex)    z__BitField_raw_clear(of, z__BitField_idTagIn(tag, bitIndex))
-#define z__BitField_toggle(of, tag, bitIndex)   z__BitField_raw_toggle(of, z__BitField_idTagIn(tag, bitIndex))
-#define z__BitField_is(of, tag, bitIndex)       z__BitField_raw_is(of, z__BitField_idTagIn(tag, bitIndex))
-#define z__BitField_iseq(of, tag, bitIndex)     z__BitField_raw_iseq(of, z__BitField_idTagIn(tag, bitIndex))
+#define z__BitField_set(of, tag, bitIndex)      z__bitf_set(of, z__BitField_idTagIn(tag, bitIndex))
+#define z__BitField_clear(of, tag, bitIndex)    z__bitf_clear(of, z__BitField_idTagIn(tag, bitIndex))
+#define z__BitField_toggle(of, tag, bitIndex)   z__bitf_toggle(of, z__BitField_idTagIn(tag, bitIndex))
+#define z__BitField_is(of, tag, bitIndex)       z__bitf_is(of, z__BitField_idTagIn(tag, bitIndex))
+#define z__BitField_iseq(of, tag, bitIndex)     z__bitf_iseq(of, z__BitField_idTagIn(tag, bitIndex))
 
 
 
