334a7ca3304b5b23e88f19d3bedc4b3dfdae9a93
zakarouf <zak.d.official@gmail.com>
2021-10-25 18:34:06 +0530

    Version bump type: 1.34 -> 1.36; prep: .14 -> .15

--- src/prep.h
+++ src/prep.h
@@ -2,7 +2,7 @@
 #define ZAKAROUF__ZPREP_H
 
 #define ZAKAROUF__ZPP_VERSION_MAJOR 0
-#define ZAKAROUF__ZPP_VERSION_MINOR 14
+#define ZAKAROUF__ZPP_VERSION_MINOR 15
 #define ZAKAROUF__ZPP_VERSION_PATCH 0
 
 #include "prep/nm/assert.h"
