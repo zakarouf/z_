3aba4be4928007fabd3c26b5c66cf5c199b41627
zakarouf <zak.d.official@gmail.com>
2021-11-05 21:38:45 +0530

    Changes: z__Dynt.size => z__Dynt.unitsize

--- src/types/dynt.h
+++ src/types/dynt.h
@@ -8,29 +8,31 @@ typedef struct __ZAKAROUF__SIMPARR_TYPE_STRUCT
 {
     z__ptr data;
     char *comment;
-    z__size size;
-    z__u32 len;
-    z__u32 lenUsed;
-    z__u8  typeID;
+    z__u32 commentLen;
+    z__size unitsize;
+    z__u32 len, lenUsed;
+    z__u8 typeID;
 
 }z__Dynt;
 
 
-z__Dynt z__Dynt_new(z__size size, z__u32 len, const char *comment, z__i32 commentLength, z__u8 typeID);
-z__Dynt z__Dynt_newFromFile(const char filepath[], z__size sizePerVal, const char *comment, z__i32 commentLength, z__u8 typeID);
-void z__Dynt_delete(z__Dynt* arrt, z__bool nameFree);
+z__Dynt z__Dynt_new(z__size unitsize, z__u32 len, const char *comment, z__i32 commentLength, z__u8 typeID);
+z__Dynt z__Dynt_newFromFile(const char filepath[], z__size unitsize, const char *comment, z__i32 commentLength, z__u8 typeID);
+void z__Dynt_delete(z__Dynt *arrt);
 void z__Dynt_push( z__Dynt *arrt, void *val);
 void z__Dynt_pop( z__Dynt *arrt);
 void z__Dynt_resize(z__Dynt *arrt, z__u32 newsize);
 z__Dynt z__Dynt_makeCopy(const z__Dynt arrt);
 
-#define z__Dynt_getValSize(arr)           (arr.size)
-#define z__Dynt_getTop(arr, T)         ( *( T *)( (arr.data) + ((arr.lenUsed-1) * arr.size)) )
-#define z__Dynt_getVal(arr, index , T) ( *( T *)( (arr.data) + (index * arr.size)) )
-#define z__Dynt_getAddress(arr, index) ((arr.data) + (index * arr.size))
-#define z__Dynt_getComment(arr)           (arr.comment)
-#define z__Dynt_getLen(arr)               (arr.len)
-#define z__Dynt_getUsed(arr)              (arr.lenUsed)
+#define z__Dynt_getUnitSize(arr)            ((arr).unitsize)
+#define z__Dynt_getRawData(arr)             ((arr).data)
+#define z__Dynt_getComment(arr)             ((arr).comment)
+#define z__Dynt_getLen(arr)                 ((arr).len)
+#define z__Dynt_getUsed(arr)                ((arr).lenUsed)
+
+#define z__Dynt_getAddress(arr, index)      (z__Dynt_getRawData(arr) + (index * z__Dynt_getUnitSize(arr)))
+#define z__Dynt_getTop(arr, T)         ( *(T*)( z__Dynt_getRawData(arr) + ((z__Dynt_getUsed(arr) - 1) * z__Dynt_getUnitSize(arr))) )
+#define z__Dynt_getVal(arr, index , T) ( *(T*)( z__Dynt_getRawData(arr) + (index * z__Dynt_getUnitSize(arr))) )
 
 
 #endif
--- src/types/ztypes_dynt.c
+++ src/types/ztypes_dynt.c
@@ -7,34 +7,40 @@
 #include "mem.h"
 
     
-z__Dynt z__Dynt_new(z__size size, z__u32 len, const char *comment, z__i32 commentLength, z__u8 typeID)
+z__Dynt z__Dynt_new(z__size unitsize, z__u32 len, const char *comment, z__i32 commentLength, z__u8 typeID)
 {
-    z__Dynt arrt;
-
-    arrt.data = malloc(size * len);
-    arrt.size = size;
-    arrt.len = len;
-    arrt.lenUsed = 0;
-    arrt.typeID = typeID;
+    z__Dynt arrt = {
+        .data = z__MALLOC(unitsize * len),
+        .unitsize = unitsize,
+        .len = len,
+        .lenUsed = 0,
+        .typeID = typeID
+    };
 
     if (commentLength == 0) {
         arrt.comment = NULL;
+        arrt.commentLen = commentLength;
     } else if (commentLength == -1) {
         arrt.comment = malloc(sizeof(char) * strnlen(comment, 1024));
         snprintf(arrt.comment, 1024, "%s", comment);
+        arrt.commentLen = 1024;
     } else {
         arrt.comment = malloc(sizeof(char) * commentLength);
         memcpy(arrt.comment, comment, commentLength);
+        arrt.commentLen = commentLength;
     }
+
 
     return arrt;
 }
+
+
 z__Dynt z__Dynt_newFromFile(const char filepath[], z__size sizePerVal, const char *comment, z__i32 commentLength, z__u8 typeID)
 {
     FILE *fp;
     if((fp = fopen(filepath, "rb")) == NULL)
     {
-        return (z__Dynt){NULL, NULL, 0, 0, 0, 0};
+        return (z__Dynt){0};
     }
 
     fseek(fp, 0, SEEK_END);
@@ -43,39 +49,38 @@ z__Dynt z__Dynt_newFromFile(const char filepath[], z__size sizePerVal, const cha
     fseek(fp, 0, SEEK_SET);  /* same as rewind(fp); */
 
     z__u64 len = fsz/sizePerVal;
-
-    void *data = malloc(len * sizePerVal);
-    fread(data, 1, fsz-1, fp);
+    
+    z__Dynt arrt = z__Dynt_new(
+            sizePerVal, fsz, comment, commentLength, typeID);
+    
+    fread(arrt.data, 1, fsz-1, fp);
     fclose(fp);
-
-    return (z__Dynt){
-         .data = data
-        ,.size = sizePerVal
-        ,.len = len
-        ,.lenUsed = (len) -1
-    };
+    
+    return arrt;
 }
 void z__Dynt_push( z__Dynt *arrt, void *val)
 {
     if (arrt->lenUsed >= arrt->len)
     {
         arrt->len += Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
-        arrt->data = z__REALLOC_SAFE(arrt->data,  arrt->size * (arrt->len) );
+        arrt->data = z__REALLOC_SAFE(arrt->data,  arrt->unitsize * (arrt->len) );
     }
     
-    void *tmpptr = (arrt->data) + (arrt->lenUsed * arrt->size);
-    memcpy(tmpptr, val, arrt->size);
+    void *tmpptr = (arrt->data) + (arrt->lenUsed * arrt->unitsize);
+    memcpy(tmpptr, val, arrt->unitsize);
     arrt->lenUsed += 1;
 }
+
 void z__Dynt_pop( z__Dynt *arrt)
 {
     arrt->lenUsed -= 1;
     if ((arrt->len - arrt->lenUsed) >= Z___TYPE_REALLOC_RESIZE_BY_DEFAULT)
     {
         arrt->len -= Z___TYPE_REALLOC_RESIZE_BY_DEFAULT;
-        arrt->data = z__REALLOC_SAFE(arrt->data,  arrt->size * (arrt->len) );
+        arrt->data = z__REALLOC_SAFE(arrt->data,  arrt->unitsize * (arrt->len) );
     }
 }
+
 inline void z__Dynt_resize(z__Dynt *arrt, z__u32 newsize)
 {
     arrt->len = newsize;
@@ -84,18 +89,27 @@ inline void z__Dynt_resize(z__Dynt *arrt, z__u32 newsize)
         arrt->lenUsed = arrt->len;
     }
 
-    arrt->data = z__REALLOC_SAFE(arrt->data, arrt->size * (arrt->len));
+    arrt->data = z__REALLOC_SAFE(arrt->data, arrt->unitsize * (arrt->len));
 }
+
+void z__Dynt_read(z__Dynt const arrt, void *dest, z__size const from, z__size const size, z__size const unitsize)
+{
+    if((arrt.unitsize - ((size * unitsize) + from)) > 0) {
+        memcpy(&arrt.data[from], dest, size * unitsize);
+    }
+}
+
 z__Dynt z__Dynt_makeCopy(const z__Dynt arrt)
 {
     z__Dynt arrtCopy = {
-        .data = malloc(arrt.len * arrt.size),
+        .data = malloc(arrt.len * arrt.unitsize),
         .len = arrt.len,
         .lenUsed = arrt.lenUsed,
-        .size = arrt.size
+        .unitsize = arrt.unitsize,
+        .commentLen = arrt.commentLen
     };
 
-    z__u32 namelen = strlen(arrt.comment);
+    z__u32 namelen = arrt.commentLen;
     if (namelen > 0)
     {
         arrtCopy.comment = malloc(sizeof(char) * namelen);
@@ -107,18 +121,16 @@ z__Dynt z__Dynt_makeCopy(const z__Dynt arrt)
     return arrtCopy;
 
 }
-inline void z__Dynt_delete(z__Dynt* arrt, z__bool nameFree)
+
+inline void z__Dynt_delete(z__Dynt* arrt)
 {
-    free(arrt->data);
+    z__FREE(arrt->data);
     arrt->len = 0;
     arrt->lenUsed = 0;
-    arrt->size = 0;
+    arrt->unitsize = 0;
 
-    if (nameFree)
-    {
-        if (arrt->comment != NULL)
-        {
-            free(arrt->comment);
-        }
+    if (arrt->commentLen > 0){
+        z__FREE(arrt->comment);
     }
+    arrt->commentLen = 0;
 }
