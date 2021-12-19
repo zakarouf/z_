3338d0a75e7206ca9e20ef190ceb3cc9644a5e1b
zakarouf <zak.d.official@gmail.com>
2021-12-19 15:42:34 +0530

    Added: prep: zpp__UNPAREN(x) makes x = (v) into v.

--- src/prep/base.h
+++ src/prep/base.h
@@ -55,6 +55,8 @@
 #define zpp__IS_PAREN_PROBE(...) zpp__DETECT_probe(~)
 #define zpp__IS_PAREN(x) zpp__DETECT_check(zpp__IS_PAREN_PROBE x)
 
+#define zpp__UNPAREN(x) zpp__EXPAND x
+
 #define zpp__From(...) ({ __VA_ARGS__  })
 
 
