1026c5adee2ab571fbceb4b50dd752428ea5771c
zakarouf <zak.d.official@gmail.com>
2021-12-25 22:47:24 +0530

    Changed: name:= _has_nodes -> has_nodes ; Removed: enum ;and Changed has_nodes' type to u8.

--- src/types/tree.h
+++ src/types/tree.h
@@ -1,7 +1,8 @@
 #ifndef ZAKAROUF__ZTYPES_TYPES__TREE_H
 #define ZAKAROUF__ZTYPES_TYPES__TREE_H
 
-typedef enum {z__Tree_tLeaf = 0, z__Tree_tNode = 1} z__Tree_tWhat;
+#include "../prep/map.h"
+#include "arr.h"
 
 #define z__PRIV__Tree_apply_pointer(x) *x
 
@@ -17,8 +18,8 @@ typedef enum {z__Tree_tLeaf = 0, z__Tree_tNode = 1} z__Tree_tWhat;
               z__PRIV__Tree_apply_pointer, zpp__UNPAREN(node_names)); \
         };                                                            \
       } nodes;                                                        \
-                            \
-      z__Tree_tWhat _has_nodes;     \
+      Name *parent_node;            \
+      z__u8 has_nodes;            \
   }
 
 
@@ -32,30 +33,31 @@ typedef enum {z__Tree_tLeaf = 0, z__Tree_tNode = 1} z__Tree_tWhat;
 #define z__Tree_val(t) (t)->val
 #define z__Tree_node(t, of) (t)->nodes.raw[of]
 #define z__Tree_nodes(t) (t)->nodes
+#define z__Tree_is_leaf(t) !(t)->has_nodes
 #define z__Tree_has_nodes(t) (t)->has_nodes
 #define z__Tree_get_nodeCount(t) (sizeof((t)->nodes.raw)/sizeof((t)->nodes.raw[0]))
 
 #define z__Tree_setas_leaf(t, ...) {            \
   z__Record_assign((&(t)->val), __VA_ARGS__);   \
-  (t)->_has_nodes = z__Tree_tLeaf;              \
+  (t)->has_nodes = false;                       \
 }
 
 #define z__Tree_setas_node(t, nd, ...) {              \
   z__Record_assign((&(t)->nodes), zpp__UNPAREN(nd));  \
   z__Record_assign((&(t)->val), __VA_ARGS__);         \
-  (t)->_has_nodes = z__Tree_tNode;                    \
+  (t)->has_nodes = true;                              \
 }
 
 #define z__Tree_match(tree)\
   for(z__typeof(tree) z__priv_tree_match_var = tree; z__priv_tree_match_var != NULL; z__priv_tree_match_var = NULL)\
-    switch((tree)->_has_nodes)
+    switch((tree)->has_nodes)
 
 #define z__Tree_slLeaf(...) \
-    break; case z__Tree_tLeaf:\
+    break; case false:\
       { z__Record_toReffrence(&z__priv_tree_match_var->val, __VA_ARGS__)
 
 #define z__Tree_slNode(n, ...)\
-    break; case z__Tree_tNode:\
+    break; case true:\
       { z__Record_toReffrence(&z__priv_tree_match_var->nodes, zpp__UNPAREN(n));\
         z__Record_toReffrence(&z__priv_tree_match_var->val, __VA_ARGS__);
 
