230e1ac43b5bbc0276aab4de4e69cb1bb9297895
zakarouf <zak.d.official@gmail.com>
2021-12-29 12:17:17 +0530

    Chore: Version Push

--- src/imp.h
+++ src/imp.h
@@ -2,8 +2,8 @@
 #define ZAKAROUF_Z_IMP_H
 
 #define ZAKAROUF__Z__IMP_VERSION_MAJOR 0
-#define ZAKAROUF__Z__IMP_VERSION_MINOR 5
-#define ZAKAROUF__Z__IMP_VERSION_PATCH 1
+#define ZAKAROUF__Z__IMP_VERSION_MINOR 10
+#define ZAKAROUF__Z__IMP_VERSION_PATCH 0
 
 #include "imp/fio.h"
 #include "imp/ext.h"
