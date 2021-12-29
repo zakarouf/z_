05a7d0e3abdf0eccec27599d84feb9511b2b38a8
zakarouf <zak.d.official@gmail.com>
2021-12-29 12:11:38 +0530

    Added: types: fnptr: z__fnptr_call(fptr, defrval, ...) => Calls a function pointer, if @fptr is null, It returns out @defrval.

--- src/types/fnptr.h
+++ src/types/fnptr.h
@@ -5,5 +5,7 @@
 
 #define z__fnptr_fn(fn) z__typeof(fn)*
 #define z__fnptr(Name, T, ...) T(*Name)(__VA_ARGS__)
+#define z__fnptr_call(fptr, defrval, ...) (fptr != NULL? (*fptr)(__VA_ARGS__) : defrval)
 
 #endif
+
