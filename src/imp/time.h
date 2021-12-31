def64fca29577359f3fc72d5157a62180d5aa559
zakarouf <zak.d.official@gmail.com>
2021-12-31 23:33:02 +0530

    Added: time: z__time_getLocalTime() => Null checking.

--- src/imp/time.h
+++ src/imp/time.h
@@ -3,7 +3,7 @@
 
 /* Time */
 void z__time_msleep(int milliseconds);
-int z__time_getLocalTime(int *h, int *m, int *s);
+int z__time_getLocalTime(int * _Nonnull h, int * _Nonnull m, int * _Nonnull s);
 
 
 #endif
