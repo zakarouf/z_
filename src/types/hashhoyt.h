37b5c5c9ec3d126512c3e3278edc86b2c1f42dd0
zakarouf <zak.d.official@gmail.com>
2022-01-07 17:30:15 +0530

    Fixed: Type casting

--- src/types/hashhoyt.h
+++ src/types/hashhoyt.h
@@ -47,7 +47,7 @@
 #define z__HashHoyt_delete(ht)\
     {                                       \
         for(z__size i; i < (ht)->len; i++) {\
-            z__FREE((ht)->entries[i].key);  \
+            z__FREE((void *)(ht)->entries[i].key); \
         }                                   \
         z__FREE((ht)->entries);             \
         (ht)->len = 0;                      \
