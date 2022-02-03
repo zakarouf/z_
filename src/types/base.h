406e9f695d4de2e07c081db945f172b984649bd5
zakarouf <zak.d.official@gmail.com>
2022-02-03 11:38:42 +0530

    Added: Type Convertion Macros.

--- src/types/base.h
+++ src/types/base.h
@@ -8,6 +8,7 @@
 
 
 /* Basic Types */
+// Unsigned
 typedef uint8_t  z__u8;
 typedef uint16_t z__u16;
 typedef uint32_t z__u32;
@@ -18,7 +19,7 @@ typedef uint_fast16_t z__u16_f;
 typedef uint_fast32_t z__u32_f;
 typedef uint_fast64_t z__u64_f;
 
-//uint_fast8_t;
+// Signed
 typedef int8_t  z__i8;
 typedef int16_t z__i16;
 typedef int32_t z__i32;
@@ -29,9 +30,11 @@ typedef int_fast16_t z__i16_f;
 typedef int_fast32_t z__i32_f;
 typedef int_fast64_t z__i64_f;
 
+// Flaats
 typedef float z__f32;
 typedef double z__f64;
 
+// Misc
 typedef int z__int;
 typedef float z__float;
 typedef bool z__bool;
@@ -47,6 +50,25 @@ typedef ptrdiff_t z__ptrdiff;
 
 #define z__sizeof sizeof
 typedef size_t z__size;
+
+// Type config_types
+#define z__i8(v) ((z__i8)(v))
+#define z__i16(v) ((z__i16)(v))
+#define z__i32(v) ((z__i32)(v))
+#define z__i64(v) ((z__i64)(v))
+
+#define z__u8(v) ((z__u8)(v))
+#define z__u16(v) ((z__u16)(v))
+#define z__u32(v) ((z__u32)(v))
+#define z__u64(v) ((z__u64)(v))
+
+#define z__int(v) ((z__int)(v))
+#define z__float(v) ((z__float)(v))
+#define z__byte(v)  ((z__byte)(v))
+#define z__bool(v)  ((z__bool)(v))
+#define z__ptr(v) ((z__ptr)(v))
+
+#define z__size(v) ((z__size)(v))
 
 #include "fnptr.h"
 
