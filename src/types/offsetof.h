2df4634deb239e6badc4f8665bb836fa6676aaab
zakarouf <zak.d.official@gmail.com>
2022-05-02 12:04:09 +0530

    Changed: Improved: offsetof

--- src/types/offsetof.h
+++ src/types/offsetof.h
@@ -1,7 +1,11 @@
 #ifndef ZAKAROUF__ZTYPES_OFFSETOF_H
 #define ZAKAROUF__ZTYPES_OFFSETOF_H
 
-#define z__offsetof(T, M) ((size_t) &((T *)0)->M)
+#if defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 5 || __GNUC__ > 3)
+#define z__offsetof(T, field) __builtin_offsetof(T, field)
+#else /* !(gcc >= 3.5) */
+#define z__offsetof(T, field) ((size_t)(&((T *)0)->field))
+#endif /* (gcc >= 3.5) */
 
 #endif
 
