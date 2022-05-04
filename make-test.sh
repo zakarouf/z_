b0c73ccd8a05abf1281b27ce155490df468fa0f2
zakarouf <zak.d.official@gmail.com>
2022-05-04 12:40:43 +0530

    Added: Test Building Script

--- make-test.sh
+++ make-test.sh
@@ -1,6 +1,21 @@
 echo "Initiating Test"
 sh ./make-lib.sh
 
+CC=gcc
+SRC="./test"
+LIB="./build/lib/libzkcollection.a"
+OUT="./build/test"
+CFILES="$(find $SRC -name '*.c')"
+
+CFLAGS="-std=c99 -ggdb -fopenmp -ftrack-macro-expansion=2 -fsanitize=address -O3"
+
+ERRFLAGS="-Wextra -Wall"
+
+$CC $CFLAGS $ERRFLAGS $CFILES $LIB -o $OUT
+
+cd build
+./test
+
 echo "Cleaning Up"
 rm -rf ./build/*
 
