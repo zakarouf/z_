43fefe7651e80835c5f7293af33216cac16a57aa
zakarouf <zak.d.official@gmail.com>
2022-05-06 22:40:55 +0530

    Chore: Arg Changed

--- src/types/bitf.h
+++ src/types/bitf.h
@@ -10,12 +10,13 @@
 #define z__BitField_id(Tag, ...)\
         enum{ zpp__Args_maplist_fn_Pattern(z__BitFieldIndex__PRIV__gen, z__BitField_idTag(Tag),, __VA_ARGS__) }
 
-#define z__bitf_set(of, bitIndex)       of |= 1 << (bitIndex)
-#define z__bitf_clear(of, bitIndex)     of &= ~(1 << (bitIndex))
-#define z__bitf_toggle(of, bitIndex)    of ^= 1 << (bitIndex)
-#define z__bitf_is(of, bitIndex)        of & (1 << (bitIndex))
-#define z__bitf_has(of, bitIndex)       (((of) >> (bitIndex))&1)
-#define z__bitf_iseq(of, bitIndex)      ((of)&(bitIndex) == bitIndex)
+#define z__bitf_set(b, offset)       b |= 1 << (offset)
+#define z__bitf_clear(b, offset)     b &= ~(1 << (offset))
+#define z__bitf_toggle(b, offset)    b ^= 1 << (offset)
+#define z__bitf_is(b, offset)        b & (1 << (offset))
+#define z__bitf_has(b, offset)       (((b) >> (offset))&1)
+#define z__bitf_iseq(b, offset)      ((b)&(offset) == offset)
+//#define z__bitf(method, b, offset)   zpp__CAT(z__bitf_, method)(b, offset)   
 
 #define z__BitField_set(of, tag, bitIndex)      z__bitf_set(of, z__BitField_idTagIn(tag, bitIndex))
 #define z__BitField_clear(of, tag, bitIndex)    z__bitf_clear(of, z__BitField_idTagIn(tag, bitIndex))
