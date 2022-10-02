9461d73f37ccda13fc23fc6affb3e121c87359e2
zakarouf <zak.d.official@gmail.com>
2022-10-02 12:04:21 +0530

    Removed: Version number, keeping seperate version for each submodule was a stupid decision

--- src/imp.h
+++ src/imp.h
@@ -1,13 +1,9 @@
 #ifndef ZAKAROUF_Z_IMP_H
 #define ZAKAROUF_Z_IMP_H
 
-#define ZAKAROUF__Z__IMP_VERSION_MAJOR 0
-#define ZAKAROUF__Z__IMP_VERSION_MINOR 10
-#define ZAKAROUF__Z__IMP_VERSION_PATCH 0
-
 #include "imp/fio.h"
 #include "imp/sys.h"
 #include "imp/time.h"
-#include "imp/ansi.h"
+#include "imp/ansifmt.h"
 //#include "imp/print.h"
 #endif
--- src/prep.h
+++ src/prep.h
@@ -1,10 +1,6 @@
 #ifndef ZAKAROUF__ZPREP_H
 #define ZAKAROUF__ZPREP_H
 
-#define ZAKAROUF__ZPP_VERSION_MAJOR 0
-#define ZAKAROUF__ZPP_VERSION_MINOR 17
-#define ZAKAROUF__ZPP_VERSION_PATCH 0
-
 #include "prep/nm/assert.h"
 #include "prep/nm/cond.h"
 #include "prep/nm/pragma.h"
