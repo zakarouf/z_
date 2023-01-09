655f08fb231d8637d7683a023e384bfdf4c08b91
zakarouf <zak.d.official@gmail.com>
2023-01-09 11:08:19 +0530

    Updated: tesh.h

--- src/imp/test.h
+++ src/imp/test.h
@@ -9,14 +9,22 @@
 #include "print.h"
 #include "ansifmt.h"
 
+#ifndef z__PRIV__test_ON_ASSERT_FALIURE
+#define z__PRIV__test_ON_ASSERT_FALIURE() goto _L_test_return;
+#endif
+
 #define z__PRIV__test_exception(fmt, ...)\
     ({ z__fprint_cl256f(stdout, 1,\
-    " =>> " __FILE__ ":" zpp__to_string(__LINE__)  ": for test %i:\"%s\", Assertion Failed!\n"\
-          z__ansi_fmt((plain))                                              \
-          fmt "\n" , _id_, _name_, ##__VA_ARGS__); _name_ = 0; goto _L_test_return; })
+    " Assertion Failed!\n"                                                     \
+    "   =>> " __FILE__ ":" zpp__to_string(__LINE__)  ": for test %i:\"%s\" \n"        \
+          z__ansi_fmt((cl256_fg, 4), (bold))                                        \
+          "Description: " z__ansi_fmt((plain)) "%s\n"                               \
+          fmt "\n" , _id_, _name_, _description_, ##__VA_ARGS__); _name_ = 0; z__PRIV__test_ON_ASSERT_FALIURE(); })
 
 #define z__test_assert(exp, fmt, ...)\
-    zpp__assert_construct(exp, z__PRIV__test_exception, "For Expression: " #exp "\n" fmt ,##__VA_ARGS__)
+    zpp__assert_construct(exp, z__PRIV__test_exception,\
+        z__ansi_fmt((bold)) "`" #exp "`\n" z__ansi_fmt((plain))\
+            fmt ,##__VA_ARGS__)
 
 #define z__test_assert_int(exp, expected) {         \
         z__typeof(exp) e1 = exp                     \
@@ -34,20 +42,23 @@
 }
 
 #define z__test_def(suite, name)\
-    static int zpp__CAT3(suite, , name)(char const * _name_, const unsigned int _id_)
+    static int zpp__CAT3(suite, , name)(char const * _name_, char const * _description_, const unsigned int _id_)
 
 #define z__test_run(suite, ...)\
     ({                                                                                  \
         const char *fn_names[] = {zpp__Args_maplist(zpp__to_string, __VA_ARGS__)};      \
-        int (*fn[])(const char *, const unsigned int) = {                               \
-                    zpp__Args_maplist_Pattern(suite,, __VA_ARGS__)                      \
+        int (*fn[])(const char *, const char *, const unsigned int) = {                 \
+                zpp__Args_maplist_Pattern(suite,, __VA_ARGS__)                          \
         };                                                                              \
         size_t passed = 0;                                                              \
         z__fprint_cl256f(stdout, 5,                                                     \
                 z__ansi_fmt((bold))                                                     \
                 "Running Suite \"" #suite "\"\n");                                      \
         for (size_t i = 0; i < zpp__Args_Count(__VA_ARGS__); i++) {                     \
-            if((fn[i])(fn_names[i], i)) {                                               \
+            z__fprint_cl256f(stdout, 4,                                                 \
+                    " * Running Test [%zu/" zpp__to_string(zpp__Args_Count(__VA_ARGS__))"] \"%s\"\n"\
+                    , i + 1, fn_names[i]);\
+            if((fn[i])(fn_names[i], "", i)) {                                           \
                 z__fprint_cl256f(stdout, 6                                              \
                     , " -> Test [%zu/"\
                                   zpp__to_string(zpp__Args_Count(__VA_ARGS__))\
