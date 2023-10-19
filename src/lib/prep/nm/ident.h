770a1c4a0373e120a44c88a84b9a18e16b5d7d9e
zakarouf <zak.d.official@gmail.com>
2023-10-19 23:18:02 +0530

    Revert "Changed: Restructure of the source code."
    This reverts commit 158f6c42e0b806f940e96d129d35a45e508aa717.

--- src/lib/prep/nm/ident.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZAKAROUF__ZPREP_NM_IDENT_H
-#define ZAKAROUF__ZPREP_NM_IDENT_H
-
-#include "../base.h"
-
-#define zpp__macIdent(name) zpp__CAT3(_priv_, __LINE__, name)
-
-#endif
-
--- src/lib/prep/nm/string.h
+++
@@ -1,7 +0,0 @@
-#ifndef ZAKAROUF__ZPREP_NM_STRING_H
-#define ZAKAROUF__ZPREP_NM_STRING_H
-
-#include "../base.h"
-#define zpp__to_string(...) zpp__PRIV_TO_STRING(__VA_ARGS__)
-
-#endif
